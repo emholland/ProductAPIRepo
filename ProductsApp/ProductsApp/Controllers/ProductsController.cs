28e3c010cda7463340eb55d4150bfe335ff9f38b
emholland <tun25675@temple.edu>
2025-04-15 22:22:58 -0400

    changed products

--- ProductsApp/ProductsApp/Controllers/ProductsController.cs
+++ ProductsApp/ProductsApp/Controllers/ProductsController.cs
@@ -11,9 +11,9 @@ namespace ProductsApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 1, Name = "Potato Soup", Category = "Grocery", Price = 2 },
+            new Product { Id = 2, Name = "Yo", Category = "Toy", Price = 3.76M },
+            new Product { Id = 3, Name = "Ham", Category = "Hard", Price = 16M }
         };
         public ProductsController()
         {
